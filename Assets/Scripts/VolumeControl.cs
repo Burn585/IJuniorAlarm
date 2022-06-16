e782caf36af1b428e20eae94b97ae2f9e2cf636d
Burn585 <levshakov.pavel@yandex.ru>
2022-06-16 15:45:15 +0300

    Change VolumeControl

--- Assets/Scripts/VolumeControl.cs
+++ Assets/Scripts/VolumeControl.cs
@@ -8,8 +8,12 @@ public class VolumeControl : MonoBehaviour
     [SerializeField] private float _speedRate = 0.5f;
 
     private AudioSource _audioSource;
-    private float _currentVolume = 0;
     private Coroutine _fadeJob;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     public void Run()
     {
@@ -20,44 +24,24 @@ public class VolumeControl : MonoBehaviour
     public void Stop()
     {
         StopCoroutine(_fadeJob);
+        StartCoroutine(ChangeVolume(0));
         _audioSource.Stop();
-    }
-
-    private void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
     }
 
     private IEnumerator FadeInOutVolume()
     {
         while (true)
         {
-            yield return StartCoroutine(UpVolume());
-            yield return StartCoroutine(DownVolume());
+            yield return StartCoroutine(ChangeVolume(1));
+            yield return StartCoroutine(ChangeVolume(0));
         }
     }
 
-    private IEnumerator UpVolume()
+    private IEnumerator ChangeVolume(float target)
     {
-        float maxVolume = 1;
-
-        while(_currentVolume < maxVolume)
+        while(_audioSource.volume != target)
         {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, maxVolume, _speedRate * Time.deltaTime);
-            _audioSource.volume = _currentVolume;
-
-            yield return null;
-        }
-    }
-
-    private IEnumerator DownVolume()
-    {
-        float minVolume = 0;
-
-        while(_currentVolume > minVolume)
-        {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, minVolume, _speedRate * Time.deltaTime);
-            _audioSource.volume = _currentVolume;
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, target, _speedRate * Time.deltaTime);
 
             yield return null;
         }
